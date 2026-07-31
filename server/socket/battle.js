ed8e8f59e1f36cec88906eb0590e6824a69fc6eb
niconico-web <dajiechishui83@gmail.com>
2026-07-31 23:19:11 +0900

    Fix battle.js socket module structure error
    - Change battle.js to use io.on connection pattern like other socket modules
    - Fix TypeError: Cannot read properties of undefined (reading 'on')

--- server/socket/battle.js
+++ server/socket/battle.js
@@ -1,79 +1,83 @@
 const BattleManager = require("../managers/BattleManager");
 const BattleEngine = require("../managers/BattleEngine");
 
-module.exports = function(io, socket){
+module.exports = function(io){
 
-    // プレイヤーの回答
-    socket.on("submitAnswer", (data) => {
-        const { roomId, answer } = data;
-        const battle = BattleManager.getBattle(roomId);
-        
-        if (!battle) {
-            socket.emit("answerError", { message: "Battle not found" });
-            return;
-        }
-        
-        const result = BattleEngine.processAnswer(battle, socket.id, answer);
-        
-        if (result.error) {
-            socket.emit("answerError", { message: result.error });
-            return;
-        }
-        
-        // 結果を両方のプレイヤーに送信
-        io.to(roomId).emit("answerResult", result);
-        
-        // バトルが終了した場合
-        if (result.winner) {
-            const finalResult = BattleEngine.finalizeBattle(battle);
-            io.to(roomId).emit("battleFinished", finalResult);
-            BattleManager.deleteBattle(roomId);
-        }
-    });
+    io.on("connection",(socket)=>{
 
-    // バトル開始（問題送信）
-    socket.on("requestBattleStart", (data) => {
-        const { roomId } = data;
-        const battle = BattleManager.getBattle(roomId);
-        
-        if (!battle) {
-            socket.emit("battleError", { message: "Battle not found" });
-            return;
-        }
-        
-        const initialization = BattleEngine.initializeBattle(battle);
-        
-        // 両方のプレイヤーに最初の問題を送信
-        io.to(roomId).emit("battleStarted", {
-            initialQuestion: initialization.initialQuestion,
-            players: battle.players
+        // プレイヤーの回答
+        socket.on("submitAnswer", (data) => {
+            const { roomId, answer } = data;
+            const battle = BattleManager.getBattle(roomId);
+            
+            if (!battle) {
+                socket.emit("answerError", { message: "Battle not found" });
+                return;
+            }
+            
+            const result = BattleEngine.processAnswer(battle, socket.id, answer);
+            
+            if (result.error) {
+                socket.emit("answerError", { message: result.error });
+                return;
+            }
+            
+            // 結果を両方のプレイヤーに送信
+            io.to(roomId).emit("answerResult", result);
+            
+            // バトルが終了した場合
+            if (result.winner) {
+                const finalResult = BattleEngine.finalizeBattle(battle);
+                io.to(roomId).emit("battleFinished", finalResult);
+                BattleManager.deleteBattle(roomId);
+            }
         });
-    });
 
-    // バトル終了
-    socket.on("battleFinished", (data) => {
+        // バトル開始（問題送信）
+        socket.on("requestBattleStart", (data) => {
+            const { roomId } = data;
+            const battle = BattleManager.getBattle(roomId);
+            
+            if (!battle) {
+                socket.emit("battleError", { message: "Battle not found" });
+                return;
+            }
+            
+            const initialization = BattleEngine.initializeBattle(battle);
+            
+            // 両方のプレイヤーに最初の問題を送信
+            io.to(roomId).emit("battleStarted", {
+                initialQuestion: initialization.initialQuestion,
+                players: battle.players
+            });
+        });
 
-        socket.to(data.roomId).emit("battleFinished", data);
+        // バトル終了
+        socket.on("battleFinished", (data) => {
 
-    });
+            socket.to(data.roomId).emit("battleFinished", data);
 
-    // 再戦リクエスト
-    socket.on("requestRematch", (data) => {
+        });
 
-        const { roomId, playerId } = data;
-        socket.to(roomId).emit("rematchRequested", { playerId });
+        // 再戦リクエスト
+        socket.on("requestRematch", (data) => {
 
-    });
+            const { roomId, playerId } = data;
+            socket.to(roomId).emit("rematchRequested", { playerId });
 
-    // 再戦受諾
-    socket.on("acceptRematch", (data) => {
+        });
 
-        const { roomId } = data;
-        // Create new room for rematch
-        const newRoomId = Math.random().toString(36).substring(2, 8).toUpperCase();
-        
-        // Notify both players to join new room
-        io.to(roomId).emit("rematchConfirmed", { newRoomId });
+        // 再戦受諾
+        socket.on("acceptRematch", (data) => {
+
+            const { roomId } = data;
+            // Create new room for rematch
+            const newRoomId = Math.random().toString(36).substring(2, 8).toUpperCase();
+            
+            // Notify both players to join new room
+            io.to(roomId).emit("rematchConfirmed", { newRoomId });
+
+        });
 
     });
 
